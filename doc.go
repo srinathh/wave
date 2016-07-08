47ff2aebde80e8c6e0d9de8a541dc6a495192692
Hariharan Srinath <srinathh@gmail.com>
2016-07-08 11:46:01 +0530

    cleaned up package doc

--- doc.go
+++ doc.go
@@ -1,4 +1,3 @@
 // Package wave implements a reader and writer for standard PCM wav files.
-// It supports reading 8, 16, 24 & 32 bit integer and 32 bit float and
-// writing 8, 16, 32 bit integer and 32 bit float wave files.
+// It supports reading 8, 16, 24 & 32 bit integer and floating point wave files.
 package wave
